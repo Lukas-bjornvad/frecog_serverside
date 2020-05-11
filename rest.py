764510156eb32b804d787e38d2343791900536ce
Mutestock <doublenegative@protonmail.com>
2020-05-11 12:58:46 +0000

    server side push

--- rest.py
+++ rest.py
@@ -1,4 +1,5 @@
 import werkzeug
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 werkzeug.cached_property = werkzeug.utils.cached_property
 from flask import Flask
@@ -11,6 +12,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = construct_con_str()
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 api = Api(app)
 ns = api.namespace("rest", description="Assignment Rest")
